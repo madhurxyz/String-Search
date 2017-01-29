b296bf6918f8d4f48442af2501df881ca6015509
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-29 08:23:08 -0800

    setting up argument input and unittests

--- string_search.py
+++ string_search.py
@@ -1,1 +1,16 @@
 #!python
+
+
+def main():
+    import sys
+    super_string = sys.argv[1]
+    print super_string
+    args = sys.argv[2:]
+    if len(args) > 0:
+        for arg in args:
+            print arg
+
+
+
+if __name__ == '__main__':
+    main()
--- test_string_search.py
+++ test_string_search.py
@@ -2,3 +2,9 @@
 
 from string_search import string_search
 import unittest
+
+class StringSearchTest(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
