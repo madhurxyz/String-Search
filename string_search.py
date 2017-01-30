1af73c65beb37168f1e1658d52cb31317cade27b
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-30 10:36:23 -0800

    built functionality to break down super string into smaller string

--- string_search.py
+++ string_search.py
@@ -8,34 +8,13 @@ def in_string(super_str, sub_str):
     # return in_string_recursive(super_str, sub_str)
 
 def in_string_iterative(super_str, sub_str):
-    # sub_l = len(sub_str)
-    # sub_index = 0
-    # for super_index in range(0, len(super_str)-1):
-    #     last_index = super_index + sub_l - 1
-    #     while last_index <= len(super_str) - 1:
-    #         for sub_index in range(super_index, last_index):
-    #             print super_str[sub_index]
-    #         last_index+=1
-
-    # sub_l = len(sub_str)
-    # sup_l = len(super_str)
-    # sub_index = 0
-    # super_index = 0
-    # last = sup_l - 1
-    # for super_index in range(0, last):
-    #     while super_index<=last:
-    #         for sub_index in range(super_index, super_index + sub_l - 1):
-    #             print super_str[super_index]
-    #         super_index += 1
-
     sup_l = len(super_str)
     sub_l = len(sub_str)
     first = 0
     last = sub_l
-    while last is not sup_l - 1:
-        for super_index in range(0, sup_l - sub_l):
-            for sub_index in range(first, last):
-                print super_str[sub_index]
+    while last is not sup_l + 1:
+        for sub_index in range(first, last):
+            print super_str[sub_index]
         first += 1
         last += 1
 
